cb4e58b62be207410ffd9178530d0b97050d71d3
Vignesh Ravindran <vrravind@uwaterloo.ca>
2021-06-23 21:53:26 -0400

    Added temp routes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDm3mxZEd08gpqEiKMyQZBzCBgT_-u1NPg",
+    authDomain: "canvas-8eec3.firebaseapp.com",
+    databaseURL: "https://canvas-8eec3-default-rtdb.firebaseio.com",
+    projectId: "canvas-8eec3",
+    storageBucket: "canvas-8eec3.appspot.com",
+    messagingSenderId: "81693423860",
+    appId: "1:81693423860:web:cdf9b4fa0cf4f95d4806a1",
+    measurementId: "G-XKM9MEQ0N0"
+  }
 };
 
 /*
