6e0771f45729bb1bf244c2c9209c659cb441bf5f
Vignesh Ravindran <vrravind@uwaterloo.ca>
2021-06-24 17:45:28 -0400

    figured out how to draw free hand

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,7 +16,7 @@ import { AuthService } from "./auth.service";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { fabric } from 'fabric';
+
 @NgModule({
   declarations: [
     AppComponent,
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -8,10 +8,33 @@ import { fabric } from 'fabric';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  canvas: any;
 
   constructor(public AuthService: AuthService) { }
 
   ngOnInit(): void {
+    this.canvas = new fabric.Canvas('canvas', {
+      width: 500,
+      height: 300,
+      isDrawingMode: false
+    });
+ 
+    this.canvas.add(new fabric.Path()); //must set isDrawingMode to true here else false
+
+    this.canvas.add(new fabric.Circle({
+      left: 200,
+      top: 100,
+      radius: 25,
+      fill: '#afa'
+    }));
+
+    this.canvas.add(new fabric.Triangle({
+      left: 300,
+      top: 100,
+      width: 50,
+      height: 50,
+      fill: '#aaf'
+    }));
   }
 
 }
