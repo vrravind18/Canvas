97df6efdb2c4bc1b49134a1ba5dad03c4bf748b0
Vignesh Ravindran <vrravind@uwaterloo.ca>
2021-06-26 17:01:07 -0400

    add styling

---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css'],
+})
+export class SignupComponent implements OnInit {
+  constructor(public authService: AuthService) {}
+
+  ngOnInit(): void {}
+}
