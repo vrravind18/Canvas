bb77c6e670f3fd736da14ec04b1be76bd62963ad
Vignesh Ravindran <vrravind@uwaterloo.ca>
2021-06-28 01:02:50 -0400

    added FireBase Hosting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDm3mxZEd08gpqEiKMyQZBzCBgT_-u1NPg",
+    authDomain: "canvas-8eec3.firebaseapp.com",
+    databaseURL: "https://canvas-8eec3-default-rtdb.firebaseio.com",
+    projectId: "canvas-8eec3",
+    storageBucket: "canvas-8eec3.appspot.com",
+    messagingSenderId: "81693423860",
+    appId: "1:81693423860:web:69724b5694b816e24806a1",
+    measurementId: "G-S80CB3JE2E"
+  }
 };
